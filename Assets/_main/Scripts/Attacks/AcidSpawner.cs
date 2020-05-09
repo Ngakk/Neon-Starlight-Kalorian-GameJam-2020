894017fd1c1f6a848494395901ba81f65a3bc3b9
Retus001 <retus001@gmail.com>
2020-05-09 18:49:24 -0500

    Acid Attack Pattern

---
+++ Assets/_main/Scripts/Attacks/AcidSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidSpawner : MonoBehaviour
+{
+    public Transform target;
+    public float spawnRate;
+    public GameObject acidIndicator;
+    public GameObject acidPfb;
+    public Vector3 spawnOffset;
+
+    public void SpawnAcid(int _ammount)
+    {
+        StartCoroutine(spawnAcidSpot(_ammount));
+    }
+
+    public IEnumerator spawnAcidSpot(int _ammount)
+    {
+        for (int i = 0; i < _ammount; i++)
+        {
+            GameObject acid = Instantiate(acidIndicator, new Vector3(target.position.x, 0.01f, target.position.z), Quaternion.identity);
+            StartCoroutine(spawnAcidDrop(acid.GetComponent<ParticleSystem>().main.duration, target.position));
+            Destroy(acid, acid.GetComponent<ParticleSystem>().main.duration + 0.1f);
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+
+    public IEnumerator spawnAcidDrop(float _delay, Vector3 _pos)
+    {
+        yield return new WaitForSeconds(_delay);
+        GameObject drop = Instantiate(acidPfb, new Vector3(_pos.x + spawnOffset.x, _pos.y + spawnOffset.y, _pos.z + spawnOffset.z), acidPfb.transform.rotation);
+        Destroy(drop, 2.0f);
+    }
+}
