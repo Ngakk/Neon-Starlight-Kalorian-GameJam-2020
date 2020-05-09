7333200fa34198238bc419788025ef1db4e5cc8d
Retus001 <retus001@gmail.com>
2020-05-08 20:12:23 -0500

    Recipes Scriptable Objects

---
+++ Assets/_main/Scripts/Systems/Recipes/Recipe_SO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+[CreateAssetMenu(fileName = "newRecipe", menuName = "Recipe")]
+
+public class Recipe_SO : ScriptableObject
+{
+    public string recipeName;
+    public InventoryItem_SO[] ingredients;
+}
