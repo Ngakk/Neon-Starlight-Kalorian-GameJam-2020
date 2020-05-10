6495633285c9daa6453b380db0634676d8ba478d
Retus001 <retus001@gmail.com>
2020-05-09 22:25:29 -0500

    Cooking Update

---
+++ Assets/_main/Scripts/Systems/Ingredients/IngredientCooking.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+public class IngredientCooking : MonoBehaviour
+{
+    public List<InventoryItem_SO> chosenIngredients = new List<InventoryItem_SO>();
+
+    public void SelectIngredient(InventoryItem_SO _ingredient)
+    {
+        if (chosenIngredients.Count < 3)
+        {
+            chosenIngredients.Add(_ingredient);
+        }
+    }
+    
+    public void DeselectIngredient(InventoryItem_SO _ingredient)
+    {
+        if (chosenIngredients.Contains(_ingredient))
+        {
+            chosenIngredients.Remove(_ingredient);
+        }
+    }
+}
