28d498df5e37e5d9fe62b451237e1a7a9bb91b22
Ngakk <ngakwastaken@gmail.com>
2020-05-10 14:21:18 -0500

    WIP debris random sprite

---
+++ Assets/_main/Scripts/Utilities/Misc/RandomSpriteChooser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpriteChooser : MonoBehaviour
+{
+    new public SpriteRenderer renderer;
+    public Sprite[] options;
+
+    public void Choose()
+    {
+        renderer.sprite = options[Random.Range(0, options.Length)];
+    }
+}
