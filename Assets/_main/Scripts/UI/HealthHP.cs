8654a19dd87272a72f2d486810772d4cbc0e37a8
Retus001 <retus001@gmail.com>
2020-05-10 21:15:01 -0500

    char hp update

--- Assets/_main/Scripts/UI/HealthHP.cs
+++ Assets/_main/Scripts/UI/HealthHP.cs
@@ -7,10 +7,27 @@ public class HealthHP : MonoBehaviour
 {
     public GameObject hpPanel;
     public Health charHP;
+    public Hurtbox hurtBox;
 
     private void Start()
     {
         ManagerStatic.uiManager.lifePanel = hpPanel;
         ManagerStatic.uiManager.charHP = charHP;
+        ManagerStatic.uiManager.UpateCharHP();
+    }
+
+    public void OnEnable()
+    {
+        hurtBox.onHurt += OnHurt;
+    }
+
+    public void OnDisable()
+    {
+        hurtBox.onHurt -= OnHurt;
+    }
+
+    public void OnHurt(HitData data)
+    {
+        ManagerStatic.uiManager.UpateCharHP();
     }
 }
