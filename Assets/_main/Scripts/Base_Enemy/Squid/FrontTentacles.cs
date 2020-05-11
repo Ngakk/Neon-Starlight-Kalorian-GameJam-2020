c902d3a66365794e9ede84db605d4752d363a957
Ngakk <ngakwastaken@gmail.com>
2020-05-10 20:35:24 -0500

    Front tentacles attack

---
+++ Assets/_main/Scripts/Base_Enemy/Squid/FrontTentacles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontTentacles : MonoBehaviour
+{
+    public Animator anim;
+    public void Attack(float _delay)
+    {
+        Invoke("AttackAnim", _delay);
+    }
+
+    void AttackAnim()
+    {
+        anim.SetTrigger("Attack");
+    }
+}
